8cdc286227fadf88b88eaa8395abadc859084b29
rjsaimon-gif <sreginajilta@gmail.com>
2024-10-09 00:47:19 -0500

    made changes to the html file and js file to setup search engine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function handleSearchSubmit(event) {
+    event.prevent.default ();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+}
 
+let searchFormElement = document.querySelector("#searcg-form")
+searchFormElement.addEventListener("submit",handleSearchSubmit)
