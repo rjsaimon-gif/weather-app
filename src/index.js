d0ba2831d70de377faf2fb7db4d6e2b89dc84012
rjsaimon-gif <sreginajilta@gmail.com>
2024-10-25 01:58:28 -0500

    made changes to the container

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-function handleSearchSubmit(event) {
-    event.prevent.default ();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
-let searchFormElement = document.querySelector("#searcg-form")
-searchFormElement.addEventListener("submit",handleSearchSubmit)
+function searchCity(city) {
+  let apiKey = "7a8aacadf32fc08tff87b375486o409c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#searcg-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
