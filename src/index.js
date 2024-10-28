6f3866d55a825e581493202668bfce9b52f64d20
rjsaimon-gif <sreginajilta@gmail.com>
2024-10-27 22:52:43 -0500

    Making updates to the HTML, CSS, and JS file to add weather forecast

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
+getForecast (response.data.city);
+
 function formatDate (date) {
     let minutes = date.getMinutes();
     let hours = date.getHours();
@@ -39,6 +41,46 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay (timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+    return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "7a8aacadf32fc08tff87b375486o409c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);  
+}
+
+function displayForecast(response) {
+    let forecastHtml = "";
+
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
+            forecastHtml =
+            forecastHtml +
+         `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
+          </div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
+        </div>
+      </div>
+    `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
